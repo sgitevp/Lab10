b0a35db3a5d954392eb140d6283b8fa2c2656e6f
Vincent <vincent_poh@ite.edu.sg>
2022-02-08 13:53:56 +0800

    update by group vincent

--- scrapyTest.py
+++ scrapyTest.py
@@ -2,7 +2,7 @@ import scrapy
 
 class NewSpider(scrapy.Spider):
     name = "new_spider"
-    start_urls = ['http://brickset.com/sets/year-2019']
+    start_urls = ['http://brickset.com/sets/year-2009']
 
     def parse(self, response):
         #xpath_selector = '//img'
